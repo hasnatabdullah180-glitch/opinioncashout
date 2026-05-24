bd0ff950da24327038bd565cfabf32682034ed42
Rakib Hasan <hasnatabdullah180@gmail.com>
2026-05-25 03:44:21 +0600

    signup route fixed

--- app/singup/page.tsx
+++ app/singup/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 export default function SignupPage() {
   return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-5xl text-green-400 font-bold">
-        Signup Page Working
-      </h1>
-    </main>
+    <div>
+      <h1>Signup Working</h1>
+    </div>
   );
 }
